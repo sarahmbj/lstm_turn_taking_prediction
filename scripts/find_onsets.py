93b5bd96d7be6c708334b732c397f427ca5fa3d3
sarahmbj <43094383+sarahmbj@users.noreply.github.com>
2020-06-09 18:11:17 +0100

    added comments

--- scripts/find_onsets.py
+++ scripts/find_onsets.py
@@ -37,7 +37,7 @@ onset_count = 0
 short_count = 0
 long_count = 0
 
-    
+# silence refers just a given speaker isn't speaking, not total silence    
 start_silence = 30 # 1.5 seconds of silence before onset
 onset_min = 10 # 500 ms of speech minimum for onset to fall into either category
 short_max_extra = 10 # if utterance is a a maximum of 500ms longer after the 500ms onset it is Short
@@ -53,27 +53,27 @@ for seq in complete_file_list:
     f_onsets = []
         
     #%% find pauses
-    pauses = ~(annotations[:,0] | annotations[:,1]) 
+    pauses = ~(annotations[:,0] | annotations[:,1]) # pulls the matrix for each dialogue into the respective speakers, and checks if anyone is speaking
     # False is when someone is speaking
     
-    for g_f,f_g in zip([0,1],[1,0]):
-        for indx in range(start_silence,len(annotations)-(short_no_speech+onset_min)):
-            if all(annotations[indx:indx+onset_min,g_f]) and not(any(annotations[indx-start_silence:indx,g_f])):
+    for g_f,f_g in zip([0,1],[1,0]): #run twice for each file - swapping speaker roles
+        for indx in range(start_silence,len(annotations)-(short_no_speech+onset_min)): # loop through all frames, but only interested in frames that could have a silence of 5 secs before them, and at least a short pause and onset of speech after them
+            if all(annotations[indx:indx+onset_min,g_f]) and not(any(annotations[indx-start_silence:indx,g_f])): #check speaker wasnt speaking before this index, and was speaking after this index
                 onset_count = onset_count + 1
-                # onset frame is 50ms after the current index
+                # onset frame is 500ms after the current index
                 onset_frame = indx + onset_min
                 # check that selection is right length and includes right info
                 selection = annotations[indx+onset_min-1:indx+onset_min+short_max_extra-1,g_f]
 #                if all(annotations[indx:indx+onset_min+long_min_extra,g_f]):
-                if annotations[indx,g_f] and annotations[indx+onset_min+long_min_extra,g_f] and \
-                    sum(1*annotations[indx:indx+onset_min+long_min_extra,g_f])>50:
-                    long_count = long_count + 1
-                    if g_f == 0: # 0 is g, 1 is f
+                if annotations[indx,g_f] and annotations[indx+onset_min+long_min_extra,g_f] and \ # if the speaker is speaking at the index, and also speaking 3s later
+                    sum(1*annotations[indx:indx+onset_min+long_min_extra,g_f])>50: #and if they are speaking for more than 50 of those frames (2.5s)
+                    long_count = long_count + 1 # then this counts as a long utterance
+                    if g_f == 0: # 0 is g, 1 is f - add the type of utterance and frame index to the list of onsets for the speaker in question
                         g_onsets.append([onset_frame,1]) # [index of onset, short(0) or long(1)]
                     else:
                         f_onsets.append([onset_frame,1])
-                elif not(len(np.where(selection[:-1]!=selection[1:])[0])==0):
-                    change_indx = np.where(selection[:-1]!=selection[1:])[0][0]
+                elif not(len(np.where(selection[:-1]!=selection[1:])[0])==0): # checking if this is a short utterance (not sure how the logic works!)
+                    change_indx = np.where(selection[:-1]!=selection[1:])[0][0] 
                     if not(any(annotations[indx+onset_min+change_indx+1:indx+onset_min+change_indx+long_min_extra+1,g_f])):
                         short_count = short_count + 1
                         if g_f == 0: # 0 is g, 1 is f

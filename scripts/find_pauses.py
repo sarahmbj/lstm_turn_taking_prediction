76883b9ff5f47eacee342ada2050b58c642b1cd6
sarahmbj <43094383+sarahmbj@users.noreply.github.com>
2020-06-09 16:53:10 +0100

    added comments

--- scripts/find_pauses.py
+++ scripts/find_pauses.py
@@ -22,7 +22,7 @@ if not(os.path.exists('./data/datasets/')):
 file_hold_shift = h5py.File('./data/datasets/hold_shift.hdf5','w')
 
 pause_str_list = ['50ms','250ms','500ms']
-pause_length_list = [1,5,10]
+pause_length_list = [1,5,10] # one frame, five frames or ten frames
 length_of_future_window = 20 # (one second) find instances where only one person speaks after the pause within this future window
 
 #%% get Annotations
@@ -33,7 +33,7 @@ frame_times_dict = {}
 for file_name in test_file_list:
     data_f = pd.read_csv(va_annotations_path+'/'+file_name+'.f.csv')
     data_g = pd.read_csv(va_annotations_path+'/'+file_name+'.g.csv')
-    annotations = np.column_stack([np.array(data_g)[:,1].astype(bool),np.array(data_f)[:,1].astype(bool)])
+    annotations = np.column_stack([np.array(data_g)[:,1].astype(bool),np.array(data_f)[:,1].astype(bool)]) #combining voice activity arrays for f and g into a matrix
     annotations_dict[file_name] = annotations
     frame_times_dict[file_name] = np.array(data_g)[:,0]
 
@@ -51,33 +51,34 @@ for pause_str, pause_length in zip(pause_str_list,pause_length_list):
     num_holds = 0
     num_shifts = 0
     
-    for seq in test_file_list:
-        annotations = annotations_dict[seq]
+    for seq in test_file_list: #looking through the list of all dialogue files
+        annotations = annotations_dict[seq] #pulls out a matrix for each dialogue in the loop
             
         #%% find pauses
-        pauses = ~(annotations[:,0] | annotations[:,1]) 
+        pauses = ~(annotations[:,0] | annotations[:,1]) #pulls the matrix for each dialogue into the respective speakers, and checks if anyone is speaking
         # False is when someone is speaking
+        # pauses is a vector of True and False for each frame of the dialogue
         
         pause_list = list()
         g_hold_shift = list()
         f_hold_shift = list()
         uncertain = list()
         
-        # pause_length = 10 # 500 ms
-        for i in range(pause_length,len(pauses)):    
-            if all(pauses[i-pause_length:i]==True) & (pauses[i-pause_length-1]==False):
-                # check if last speech frame had speech by both speakers
-                if (annotations[i-pause_length-1,0] & annotations[i-pause_length-1,1]):
+        
+        for i in range(pause_length,len(pauses)):    #e.g. if pause_length = 10 (for 500ms pause), will loop through indices from frame 10 to the end
+            if all(pauses[i-pause_length:i]==True) & (pauses[i-pause_length-1]==False): #second condition checks someone was speaking before the pause (i.e. it isn't a longer pause)
+                
+                if (annotations[i-pause_length-1,0] & annotations[i-pause_length-1,1]): # check if last speech frame had speech by both speakers
                     uncertain.append(i)
                 else:
                     pause_list.append(i) # start calculating who takes turn from i (inclusive)
                         
-        pause_seq_list.append(pause_list)
+        pause_seq_list.append(pause_list) # will be a list, containing a list of pause indexes (i.e. frame number) for each dialogue
         list_sum +=len(pause_list)
         uncertain_sum += len(uncertain)
         
-        for i in pause_list:
-            if annotations[i-pause_length-1,0]:
+        for i in pause_list: #for each pause, works out if it is a hold or a shift
+            if annotations[i-pause_length-1,0]: #this if statement works out who current speaker is (0 or 1 refers to speaker)
                 if (any(annotations[i:i+length_of_future_window,0]) and not(any(annotations[i:i+length_of_future_window,1]))):
                     g_hold_shift.append([i,0]) # [index of pause, hold(0) or shift(1)]
                     num_holds += 1
@@ -86,7 +87,7 @@ for pause_str, pause_length in zip(pause_str_list,pause_length_list):
                     g_hold_shift.append([i,1]) # [index of pause, hold(0) or shift(1)]
                     num_shifts += 1
                     turns_sum += 1
-            elif annotations[i-pause_length-1,1]:
+            elif annotations[i-pause_length-1,1]: #this if statement works out who current speaker is (0 or 1 refers to speaker)
                 if any(annotations[i:i+length_of_future_window,1]) and not(any(annotations[i:i+length_of_future_window,0])):
                     f_hold_shift.append([i,0]) # [index of pause, hold(0) or shift(1)]
                     num_holds += 1
@@ -102,7 +103,7 @@ for pause_str, pause_length in zip(pause_str_list,pause_length_list):
         f_hold_shift_list.append(f_hold_shift)
         
     print(pause_str)
-    print('num of instances: {}'.format(list_sum))  
+    print('num of instances: {}'.format(list_sum))  #total pauses across all dialogues (apart from uncertains)
     print('num Uncertain: {}'.format(uncertain_sum))
     print('num of instances where only one speaker continued: {}'.format(turns_sum))
     print('num holds: {}'.format(num_holds))
@@ -114,7 +115,7 @@ for pause_str, pause_length in zip(pause_str_list,pause_length_list):
     dset_stats = file_hold_shift[pause_str+'/stats/num_instances_total'] = list_sum
     dset_stats = file_hold_shift[pause_str+'/stats/num_instances_one_speaker_continues'] = turns_sum
     
-    for seq,indx in zip(test_file_list,range(len(test_file_list))):
+    for seq,indx in zip(test_file_list,range(len(test_file_list))): # writes a file for each dialogue (seq) and speaker (indx) of where the speaker is holding and shifting
         file_hold_shift.create_dataset(pause_str+'/hold_shift/'+seq+'/'+'g',data=np.array(g_hold_shift_list[indx]))
         file_hold_shift.create_dataset(pause_str+'/hold_shift/'+seq+'/'+'f',data=np.array(f_hold_shift_list[indx]))
 

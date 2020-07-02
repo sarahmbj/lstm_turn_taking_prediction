82fbda3e3936bb58bd70b61f84a79c296a6a39e2
sarahburnejames <sarahmbj@gmail.com>
2020-07-02 14:09:21 +0100

    refactored to create get_f_g_stats() procedure

--- inspect_results.py
+++ inspect_results.py
@@ -3,7 +3,6 @@ import pickle
 import numpy as np
 from pprint import pprint
 
-
 file_to_inspect = "./results/sample_results/results.p"
 
 with open(file_to_inspect, "rb") as results_file:
@@ -13,32 +12,38 @@ print(len(results))
 print(type(results))
 print(results.keys())
 
-# print(results['indiv_perf']) # a list of dictionaries - is it a dictionary for each run of the experiment?
+print(results['indiv_perf']) # a list of dictionaries - is it a dictionary for each run of the experiment?
 print(results['indiv_perf'][0]) # these are the labels and values for the per speaker bar chart - mean absolute error per speaker
 # would need to decide best epoch, and then get the mean error for f and for g by aggregating across all speakers
 #but do you want to report other values e.g. f-scores for things like in roddy paper? that wont be available here
 # would really like to do error curve - but that is tricky (is the reporting at different delays a less granular version of that?
 
-print(results['indiv_perf'][0]['bar_chart_vals'])
+def get_f_g_stats(pickled_results):
+    """Takes in pickled results file, prints stats about mean absolute error for f (instruction followers) and or g
+    (instruction givers) and for all speakers."""
+    with open(pickled_results, "rb") as results_file:
+        results = pickle.load(results_file)
 
-both_errors = results['indiv_perf'][0]['bar_chart_vals']
-f_errors = []  # 0,2,4,6 etc.
-g_errors = []  # 1,3,5,7 etc.
+    all_errors = results['indiv_perf'][0]['bar_chart_vals']
+    f_errors = []  # 0,2,4,6 etc.
+    g_errors = []  # 1,3,5,7 etc.
 
-for index, error in enumerate(results['indiv_perf'][0]['bar_chart_vals']):
-    if index % 2 == 0:
-        f_errors.append(error)
-    else:
-        g_errors.append(error)
+    for index, error in enumerate(results['indiv_perf'][0]['bar_chart_vals']):
+        if index % 2 == 0:
+            f_errors.append(error)
+        else:
+            g_errors.append(error)
 
-mean_f_error = sum(f_errors)/len(f_errors)
-mean_g_error = sum(g_errors)/len(g_errors)
-mean_error = sum(both_errors)
-std_f_error = np.std(f_errors)
-std_g_error = np.std(g_errors)
-std_error = np.std(both_errors)
+    mean_f_error = sum(f_errors)/len(f_errors)
+    mean_g_error = sum(g_errors)/len(g_errors)
+    mean_all_error = sum(all_errors)
+    std_f_error = np.std(f_errors)
+    std_g_error = np.std(g_errors)
+    std_all_error = np.std(all_errors)
 
-print(f'average error on f: {mean_f_error} standard deviation: {std_f_error}')
-print(f'average error on f: {mean_g_error} standard deviation: {std_g_error}')
-print(f'average error on f: {mean_error} standard deviation: {std_error}')
-print(f'Difference between average error f and g: {abs(mean_f_error-mean_g_error)}')
+    print(f'average error on f: {mean_f_error} standard deviation: {std_f_error}')
+    print(f'average error on f: {mean_g_error} standard deviation: {std_g_error}')
+    print(f'average error on f: {mean_all_error} standard deviation: {std_all_error}')
+    print(f'Difference between average error f and g: {abs(mean_f_error-mean_g_error)}')
+
+get_f_g_stats(file_to_inspect)

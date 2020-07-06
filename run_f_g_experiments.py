8921463b0837f9b222f9c33d5d3b13a95e39ec4c
sarahburnejames <sarahmbj@gmail.com>
2020-07-06 16:34:42 +0100

    debugging run_additional_test()

--- run_f_g_experiments.py
+++ run_f_g_experiments.py
@@ -546,7 +546,7 @@ for params in param_list:
     except FileNotFoundError:
         py_env = '/Users/sarahburnejames/miniconda3/bin/python'
         run_trial(params)
-    # run_additional_test(params, test_on_f=True, test_on_g=True) #TODO: check where this stores results to
+    run_additional_test(params, test_on_f=True, test_on_g=True) #TODO: check where this stores results to
     # run_additional_test(params, test_on_f=True, test_on_g=False) #TODO: check where this stores results to
     # run_additional_test(params, test_on_f=False, test_on_g=True) #TODO: check where this stores results to
 

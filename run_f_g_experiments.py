15b2ea41089c1c26166a6ad71d62284d3ef5bb46
sarahburnejames <sarahmbj@gmail.com>
2020-07-06 14:05:39 +0100

    debugging batch error

--- run_f_g_experiments.py
+++ run_f_g_experiments.py
@@ -55,7 +55,7 @@ Acous_10ms_Ling_50ms_ftrain = { #TODO: add in f_train/g_train settings to all th
     'hidden_nodes_master': 50,
     'hidden_nodes_acous': 50,
     'hidden_nodes_visual': 50,
-    'train_on_f': True,
+    'train_on_f': True, # TODO: set these all back to correct values after debugging
     'train_on_g': False,
     'test_on_f': True,
     'test_on_g': True

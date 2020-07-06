48c88580accbafa52b6c60490174e9c8ab89fdc5
sarahburnejames <sarahmbj@gmail.com>
2020-07-06 17:38:28 +0100

    debugging batch error

--- run_f_g_experiments.py
+++ run_f_g_experiments.py
@@ -58,7 +58,7 @@ Acous_10ms_Ling_50ms_ftrain = {
     'train_on_f': True, # TODO: set these all back to correct values after debugging
     'train_on_g': False,
     'test_on_f': True,
-    'test_on_g': True
+    'test_on_g': False
 }
 
 Acous_10ms_Ling_50ms_gtrain = {

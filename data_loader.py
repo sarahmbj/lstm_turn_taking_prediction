8c0c5f580f4d196c4abd7b5bc13e8051fa062c02
sarahburnejames <sarahmbj@gmail.com>
2020-07-07 21:01:10 +0100

    debugging batch error

--- data_loader.py
+++ data_loader.py
@@ -592,10 +592,10 @@ class TurnPredictionDataset(Dataset):
                 else:
                     print("before", torch.FloatTensor(self.dataset[idx]['x'][mod]).size())
                     try:
-                        output_list.append(torch.squeeze(torch.FloatTensor(self.dataset[idx]['x'][mod]), dim=3))
+                        output_list.append(torch.squeeze(torch.FloatTensor(self.dataset[idx]['x'][mod])), dim=3)
                         print('AFTER TRY (squeeze dim 3): ', (torch.FloatTensor(self.dataset[idx]['x'][mod]).size()))
                     except IndexError:
-                        output_list.append(torch.unsqueeze(torch.FloatTensor(self.dataset[idx]['x'][mod]), dim=0))
+                        output_list.append(torch.unsqueeze(torch.FloatTensor(self.dataset[idx]['x'][mod])), dim=0)
                         print('AFTER EXCEPT (just append): ', (torch.FloatTensor(self.dataset[idx]['x'][mod]).size()))
             else:
                 output_list.append([])

37243870fd80825f04daccf0a6a1f9b590d678b5
sarahburnejames <sarahmbj@gmail.com>
2020-07-13 14:55:07 +0100

    added create_results_directory()

--- test_on_existing_model_old.py
+++ test_on_existing_model_old.py
@@ -18,6 +18,11 @@ p_memory = True
 train_batch_size = 128
 
 
+def create_results_directory(directory, test_set, experiment_path):
+    print(f"{directory}/{test_set}/{experiment_path}")
+    # os.mkdirs(f"{directory}/{test_set}/{experiment_path}")
+
+
 def load_args(args_path):
     with open(args_path, "rb") as json_file:
         args_string = json.load(json_file)
@@ -74,23 +79,13 @@ def load_test_set(args_dict, test_on_g=True, test_on_f=True):
 trial_path = './two_subnets_complete/1_Acous_50ms_Ling_50ms'
 test_path = f'{trial_path}/test'
 
-print(trial_path)
-
 for directory in os.listdir(test_path):
-    print("INSIDE FOR LOOP")
-    model_save = f'{test_path}/{directory}/model.p'
-    print(model_save)
+    model_path = f'{test_path}/{directory}/model.p'
     settings_path = f'{test_path}/{directory}/settings.json'
-    print(settings_path)
     args = load_args(settings_path)
-    test_set, test_loader = load_test_set(args)
-    model = load_model(model_save, args, test_set)
-
-
-
-# model = load_model('results/example_pickled_model/model.p', 'results/example_pickled_model/settings.json')
-# test_dataset, test_dataloader = load_test_set(test_on_g=True, test_on_f=True)
-
+    test_set, test_loader = load_test_set(args, test_on_g=True, test_on_f=True)
+    model = load_model(model_path, args, test_set)
+    create_results_directory(trial_path, 'test_on_both', directory)
 
 
 

8ac367e6ab70a5877b4c744bf94f3fdaee0f608c
sarahburnejames <sarahmbj@gmail.com>
2020-07-20 17:05:46 +0100

    debugging

--- test_on_existing_model_old.py
+++ test_on_existing_model_old.py
@@ -421,10 +421,8 @@ if __name__ == "__main__":
         # perform test on loaded model
         model.eval()
         test_results = test(model, test_set, test_loader) #TODO: fix onset evaluation (needs train_results_dict)
-        print(type(test_results))
-        json_test_results = json.dumps(test_results)
         with open(results_path + '/results.txt', 'w') as file:
-            file.write(json_test_results)
+            file.write(str(test_results))
         pickle.dump(test_results, open(results_path + '/results.p', 'wb'))
         plot_person_error(test_results['indiv_perf'][-1]['bar_chart_labels'],
                           test_results['indiv_perf'][-1]['bar_chart_vals'], results_path,

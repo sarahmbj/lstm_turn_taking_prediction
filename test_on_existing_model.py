ee4510b298877e9ed4a7b08ee17e1a7e266b9537
sarahburnejames <sarahmbj@gmail.com>
2020-08-03 14:08:20 +0100

    change to test on best_model.p

--- test_on_existing_model.py
+++ test_on_existing_model.py
@@ -295,10 +295,9 @@ def test(model, test_dataset, test_dataloader, train_results_dict, onset_test_fl
         # print(out_test)
         print(len(y_test[0]), len(y_test_list))
         # print(y_test[0])
-        quit() #TODO: remove this
+        # quit() #TODO: remove this
 
-        # will need to take
-        out_test_list.extend(out_test)
+        out_test_list.append(out_test)
         y_test_list.extend(y_test)
 
         for file_name, g_f_indx, time_indices, batch_indx in zip(file_name_list,
@@ -500,7 +499,7 @@ def test_on_existing_models(trial_path, test_on_g=True, test_on_f=True, trained_
     for directory in os.listdir(test_path):
         test_set_name = get_test_set_name(test_on_f, test_on_g)
         # paths to stored models, settings, and location for new results
-        model_path = f'{test_path}/{directory}/model.p' #TODO change back to best_model.p
+        model_path = f'{test_path}/{directory}/best_model.p'
         settings_path = f'{test_path}/{directory}/settings.json'
         results_path = f"{trial_path}/{test_set_name}/{directory}"
 
@@ -555,7 +554,7 @@ def test_on_existing_models(trial_path, test_on_g=True, test_on_f=True, trained_
         json.dump(combined_results, open(trial_path + f'/report_dict_{test_set_name}.json', 'w'), indent=4, sort_keys=True)
 
 if __name__ == "__main__":
-    trial_path = './two_subnets_complete/1_Acous_50ms_Ling_50ms'
+    trial_path = './no_subnets/1_Acous_50ms'
     test_on_existing_models(trial_path, test_on_f=True, test_on_g=True)
     # test_on_existing_models(trial_path, test_on_f=False, test_on_g=True)
     # test_on_existing_models(trial_path, test_on_f=True, test_on_g=False)

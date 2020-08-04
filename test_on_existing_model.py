ef3d526c3d651ab9c8ca1e353d0e3f7850534a32
sarahburnejames <sarahmbj@gmail.com>
2020-08-04 17:08:59 +0100

    debug onset f1 score

--- test_on_existing_model.py
+++ test_on_existing_model.py
@@ -556,8 +556,8 @@ def test_on_existing_models(trial_path, test_on_g=True, test_on_f=True, trained_
 
 if __name__ == "__main__":
     trial_path = './no_subnets/2_Acous_10ms'
-    test_on_existing_models(trial_path, test_on_f=True, test_on_g=True)
-    # test_on_existing_models(trial_path, test_on_f=False, test_on_g=True)
+    # test_on_existing_models(trial_path, test_on_f=True, test_on_g=True)
+    test_on_existing_models(trial_path, test_on_f=False, test_on_g=True)
     # test_on_existing_models(trial_path, test_on_f=True, test_on_g=False)
 
     # trial_path = './two_subnets_complete/2_Acous_10ms_Ling_50ms'

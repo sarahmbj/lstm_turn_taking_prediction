f98e84dcabd872868326a0affd8d1e9d41f63b3f
sarahburnejames <sarahmbj@gmail.com>
2020-08-20 13:59:45 +0100

    create file

--- switchboard_determine_data_split.py
+++ switchboard_determine_data_split.py
@@ -2,8 +2,9 @@ import os
 import xml.etree.ElementTree as ET
 
 dialogue_tree = ET.parse("/group/corpora/public/switchboard/nxt/xml/corpus-resources/dialogues.xml")
-root = dialogue_tree.getroot()
+dialogue_root = dialogue_tree.getroot()
 # /group/corpora/public/switchboard/nxt/xml/corpus-resources/speakers.xml
 
-for child in root:
-    print(child)
+for dialogue in dialogue_root:
+    print(dialogue.tag, dialogue.attrib)
+    

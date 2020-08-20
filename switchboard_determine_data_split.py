8a203f87976ef043ae8f38c32ad60f3a1287669c
sarahburnejames <sarahmbj@gmail.com>
2020-08-20 12:04:42 +0100

    create file

--- switchboard_determine_data_split.py
+++ switchboard_determine_data_split.py
@@ -7,5 +7,5 @@ conversation_list = os.listdir(wav_directory)
 print(conversation_list)
 
 for file in conversation_list:
-    wav_file = wave.open(f'{wav_directory}/file')
+    wav_file = wave.open(f'{wav_directory}/{file}')
     print(dir(wav_file))

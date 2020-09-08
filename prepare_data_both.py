f39249173f80fbbc50b4980370b3942d04aa7246
sarahburnejames <sarahmbj@gmail.com>
2020-09-08 14:24:19 +0100

    version that only runs the experiments that use linguistic features (i.e. don't run the acous only models)

---
+++ prepare_data_both.py
@@ -0,0 +1,17 @@
+import os
+
+# os.system('python scripts/extract_gemaps.py 0')
+# os.system('python scripts/extract_gemaps.py 1')
+# os.system('python scripts/prepare_gemaps.py 0') CAN USE PREPARED GEMAPS FROM INDIVIDUAL DATA SETS
+# os.system('python scripts/prepare_gemaps.py 1')
+# os.system('python scripts/get_word_timings.py') USE OUTPUT FILES FROM INDIVIDUAL DATA SETS INSTEAD
+os.system('python scripts/prepare_fast_data_acous.py')
+os.system('python scripts/get_word_annotations_both.py 0')
+os.system('python scripts/get_word_annotations_both.py 1')
+os.system('python scripts/get_averaged_annotations.py 0')
+os.system('python scripts/get_averaged_annotations.py 1')
+os.system('python scripts/prepare_fast_data_ling.py 0')
+os.system('python scripts/prepare_fast_data_ling.py 1')
+os.system('python scripts/find_pauses.py')
+os.system('python scripts/find_overlaps.py')
+os.system('python scripts/find_onsets.py')
--- run_icmi.sh
+++ run_icmi.sh
@@ -1,3 +1,5 @@
 python ./icmi_18_results_two_subnets.py
+python ./icmi_18_results_no_subnets_lingonly.py
 #python ./icmi_18_results_no_subnets.py
+
 

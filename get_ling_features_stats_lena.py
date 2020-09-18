b2aa2036155cea6d077f5de00477a9d005b3c33c
sarahburnejames <sarahmbj@gmail.com>
2020-09-18 16:31:25 +0100

    debugging

--- get_ling_features_stats_lena.py
+++ get_ling_features_stats_lena.py
@@ -58,7 +58,6 @@ for filename in files_to_include:
     csv = pd.read_csv(words_folder + filename, usecols=['word'])
 
     if filename not in conv_vecs:
-        print("if filename not in conv_vecs")
         conv_vecs[filename] = [0]*len(ix_to_word)
 
     for x in csv['word']:
@@ -77,9 +76,7 @@ for filename in files_to_include:
 
 print(len(conv_vecs))
 print(len(conv_dict))
-print(conv_dict)
 print(len(total_dict))
-print(total_dict)
 
 pickle.dump(conv_vecs, open(base_path + 'conv_vectors.p', 'wb'))
 pickle.dump(conv_dict, open(base_path + 'conv_count_dict.p', 'wb'))

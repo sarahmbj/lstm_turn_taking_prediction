4eec187f0af6f7c179dd646b8f70291ab6b78a35
sarahburnejames <sarahmbj@gmail.com>
2020-09-18 21:08:36 +0100

    add command line args

--- get_ling_features_stats_lena.py
+++ get_ling_features_stats_lena.py
@@ -19,10 +19,11 @@ base_path = './data/extracted_annotations/'
 print(sys.argv)
 quit()
 words_folder = base_path + 'words_advanced_50ms_averaged/'
-conversations_list = 'complete'
+conversations_list = sys.argv[1] # complete, testing or training
 conversations_list_file = f'./data/splits/{conversations_list}.txt'
-# include_f = sys.argv[]False
-# include_g = True
+include_f = bool(sys.argv[2]) # Bool
+include_g = bool(sys.arv[3]) #Bool
+
 
 # get list of file names to consider for the stats
 conversations_to_include = []

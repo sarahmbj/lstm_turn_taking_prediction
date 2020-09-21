e14cfdc772bb8bd0942088078602798801b4fcd3
sarahburnejames <sarahmbj@gmail.com>
2020-09-21 17:28:59 +0100

    change selected gpu

--- run_icmi.sh
+++ run_icmi.sh
@@ -1,5 +1,5 @@
-python ./icmi_18_results_two_subnets.py 1
-python ./icmi_18_results_no_subnets_lingonly.py 1
-#python ./icmi_18_results_no_subnets.py 1
+python ./icmi_18_results_two_subnets.py 2
+python ./icmi_18_results_no_subnets_lingonly.py 2
+#python ./icmi_18_results_no_subnets.py 2
 
 

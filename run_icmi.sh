ee19b296ef69263098e19c0fba11a41a05806bb9
sarahburnejames <sarahmbj@gmail.com>
2020-09-21 17:22:26 +0100

    allow gpu core to be selected in command line arg

--- run_icmi.sh
+++ run_icmi.sh
@@ -1,6 +1,5 @@
-# $1 should be the desired gpu core to run on
-python ./icmi_18_results_two_subnets.py "$1"
-python ./icmi_18_results_no_subnets_lingonly.py "$1"
-#python ./icmi_18_results_no_subnets.py "$1
+python ./icmi_18_results_two_subnets.py 1
+python ./icmi_18_results_no_subnets_lingonly.py 1
+#python ./icmi_18_results_no_subnets.py 1
 
 

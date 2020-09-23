eace746a344da410847ef226f2a848fdaa205f4e
sarahburnejames <sarahmbj@gmail.com>
2020-09-23 22:17:14 +0100

    test on both_sets models

--- test_on_existing_model.py
+++ test_on_existing_model.py
@@ -684,7 +684,7 @@ if __name__ == "__main__":
                             test_on_f=True, test_on_g=True, report_dict_name=f'test_on_{test_data}')
 
     train_data = 'data'
-    test_data = 'matpask_data'
+    test_data = 'maptask_data'
     trial_path = './no_subnets/3_Ling_50ms'
     test_on_existing_models(trial_path, test_data_dir=test_data, train_data_dir=train_data,
                             test_on_f=True, test_on_g=True, report_dict_name=f'test_on_{test_data}')

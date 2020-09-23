ed041dfdf93a3d7768ddc2e1fc1c3f9d8e030eb6
sarahburnejames <sarahmbj@gmail.com>
2020-09-23 17:13:07 +0100

    allow cross corpus testing

--- test_on_existing_model.py
+++ test_on_existing_model.py
@@ -663,12 +663,10 @@ if __name__ == "__main__":
 
     trial_path = './no_subnets/3_Ling_50ms'
     test_on_existing_models(trial_path, test_data_dir=test_data, train_data_dir=train_data,
-                            test_on_f=True, test_on_g=True, report_dict_name='test_on_maptask',
-                            test_list_path="./maptask_data/splits/testing.txt")
+                            test_on_f=True, test_on_g=True, report_dict_name='test_on_maptask')
     trial_path = './two_subnets/2_Acous_10ms_Ling_50ms'
     test_on_existing_models(trial_path, test_data_dir=test_data, train_data_dir=train_data,
-                            test_on_f=True, test_on_g=True, report_dict_name='test_on_maptask',
-                            test_list_path="./maptask_data/splits/testing.txt")
+                            test_on_f=True, test_on_g=True, report_dict_name='test_on_maptask')
 
 #
 #     trial_path = './no_subnets/3_Ling_50ms'

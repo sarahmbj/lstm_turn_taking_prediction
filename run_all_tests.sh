cf893b2e56f6a6652a394bc48f3ec15557298b99
sarahburnejames <sarahmbj@gmail.com>
2020-09-24 15:35:49 +0100

    script to run all tests on existing models

---
+++ run_all_tests.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+cd ../dev
+python maptask_tests.py
+python onset_prediction_length_tests.py
+
+cd ../maptask_unk
+python maptask_unk_tests.py
+
+cd ../switchboard_dev
+python switchboard_tests.py
+python onset_prediction_length_tests.py
+
+cd ../switchboard_maptask_unk
+python switchboard_unk_tests.py
+
+cd ../both_sets
+python both_tests.py
+
+cd ../both_unk
+python both_unk_tests.py

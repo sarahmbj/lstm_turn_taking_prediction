4c0a2b0362ec7d8880bc54f0758aa439309f7364
sarahburnejames <sarahmbj@gmail.com>
2020-09-24 13:32:57 +0100

    add cross-corpus acoustic model tests

--- maptask_tests.py
+++ maptask_tests.py
@@ -49,7 +49,7 @@ test_on_existing_models(trial_path, test_on_f=True, test_on_g=False, trained_on_
                         test_data_dir=test_data, train_data_dir=train_data)
 
 # models trained on g only
-trial_path = './f_and_g_no_subnets/3_Acous_10ms_ftrain'
+trial_path = './f_and_g_no_subnets/4_Acous_10ms_gtrain'
 test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, trained_on_f=True, trained_on_g=False,
                         test_data_dir=test_data, train_data_dir=train_data)
 test_on_existing_models(trial_path, test_on_f=False, test_on_g=True, trained_on_f=True, trained_on_g=False,
@@ -72,3 +72,35 @@ test_on_existing_models(trial_path, test_on_f=False, test_on_g=True, trained_on_
                         test_data_dir=test_data, train_data_dir=train_data)
 test_on_existing_models(trial_path, test_on_f=True, test_on_g=False, trained_on_f=False, trained_on_g=True,
                         test_data_dir=test_data, train_data_dir=train_data)
+
+# cross corpus tests - acoustic only models
+trial_path = './no_subnets/2_Acous_10ms'
+
+train_data = 'data'
+test_data = 'switchboard_data'
+test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data)
+
+train_data = 'data'
+test_data = 'both_data'
+test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data)
+
+
+trial_path = './f_and_g_no_subnets/4_Acous_10ms_gtrain'
+
+train_data = 'data'
+test_data = 'switchboard_data'
+test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data)
+
+train_data = 'data'
+test_data = 'both_data'
+test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data)
+
+trial_path = './f_and_g_no_subnets/3_Acous_10ms_ftrain'
+
+train_data = 'data'
+test_data = 'switchboard_data'
+test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data)
+
+train_data = 'data'
+test_data = 'both_data'
+test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data)

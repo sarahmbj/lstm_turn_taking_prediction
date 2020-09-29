15d0c4e4b730f6eedfaf4b530bf6cf91ed383fd8
sarahburnejames <sarahmbj@gmail.com>
2020-09-29 13:42:29 +0100

    comment some tests

--- both_tests.py
+++ both_tests.py
@@ -9,18 +9,18 @@ test_data = 'data'
 # models trained on both roles
 trial_path = './no_subnets/2_Acous_10ms'
 test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data)
-test_on_existing_models(trial_path, test_on_f=False, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data)
-test_on_existing_models(trial_path, test_on_f=True, test_on_g=False, test_data_dir=test_data, train_data_dir=train_data)
+# test_on_existing_models(trial_path, test_on_f=False, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data)
+# test_on_existing_models(trial_path, test_on_f=True, test_on_g=False, test_data_dir=test_data, train_data_dir=train_data)
 
 trial_path = './no_subnets/3_Ling_50ms'
 test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data)
-test_on_existing_models(trial_path, test_on_f=False, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data)
-test_on_existing_models(trial_path, test_on_f=True, test_on_g=False, test_data_dir=test_data, train_data_dir=train_data)
+# test_on_existing_models(trial_path, test_on_f=False, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data)
+# test_on_existing_models(trial_path, test_on_f=True, test_on_g=False, test_data_dir=test_data, train_data_dir=train_data)
 
 trial_path = './two_subnets/2_Acous_10ms_Ling_50ms'
 test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data)
-test_on_existing_models(trial_path, test_on_f=False, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data)
-test_on_existing_models(trial_path, test_on_f=True, test_on_g=False, test_data_dir=test_data, train_data_dir=train_data)
+# test_on_existing_models(trial_path, test_on_f=False, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data)
+# test_on_existing_models(trial_path, test_on_f=True, test_on_g=False, test_data_dir=test_data, train_data_dir=train_data)
 
 # cross corpus tests - acoustic only models
 trial_path = './no_subnets/2_Acous_10ms'

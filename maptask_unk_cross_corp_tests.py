f33eab196882306b6d3e659c15fbdca8b627a6d2
sarahburnejames <sarahmbj@gmail.com>
2020-09-30 14:53:09 +0100

    run test by test

--- maptask_unk_cross_corp_tests.py
+++ maptask_unk_cross_corp_tests.py
@@ -9,17 +9,23 @@ from test_on_existing_model import test_on_existing_models
 
 #still to run:
 
-train_data = 'data'
-test_data = 'switchboard_data'
-trial_path = './no_subnets/3_Ling_50ms'
-test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data,
-                        report_dict_name="test_on_switchboard_data")
+# train_data = 'data'  #TODO: why is onset_thresh 0.0?
+# test_data = 'switchboard_data'
+# trial_path = './no_subnets/3_Ling_50ms'
+# test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data,
+#                         report_dict_name="test_on_switchboard_data")
 
 # train_data = 'data'  #TODO: why is onset_thresh 0.0?
 # test_data = 'switchboard_data'
 # trial_path = './no_subnets/2_Acous_10ms'
 # test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data,
 #                         report_dict_name="test_on_switchboard_data")
+
+train_data = 'data'
+test_data = 'switchboard_data'
+trial_path = './two_subnets/2_Acous_10ms_Ling_50ms'
+test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data,
+                        report_dict_name="test_on_switchboard_data")
 
 # train_data = 'data'
 # test_data = 'both_data'
@@ -34,8 +40,4 @@ test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_di
 
 
 
-train_data = 'data'
-test_data = 'switchboard_data'
-# trial_path = './two_subnets/2_Acous_10ms_Ling_50ms'
-# test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data,
-#                         report_dict_name="test_on_switchboard_data")
+

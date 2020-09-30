01d4668c159aded0aa10c7c7ab4cbe7fd0db53d1
sarahburnejames <sarahmbj@gmail.com>
2020-09-30 14:33:20 +0100

    run test by test

--- maptask_unk_cross_corp_tests.py
+++ maptask_unk_cross_corp_tests.py
@@ -1,18 +1,13 @@
 from test_on_existing_model import test_on_existing_models
 
-train_data = 'data'
-test_data = 'both_data'
-trial_path = './no_subnets/2_Acous_10ms'
-test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data,
-                        report_dict_name="test_on_combined_data")
+#ran succesfully:
+# train_data = 'data'
+# test_data = 'both_data'
+# trial_path = './no_subnets/2_Acous_10ms'
+# test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data,
+#                         report_dict_name="test_on_combined_data")
 
-trial_path = './no_subnets/3_Ling_50ms'
-test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data,
-                        report_dict_name="test_on_combined_data")
-
-trial_path = './two_subnets/2_Acous_10ms_Ling_50ms'
-test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data,
-                        report_dict_name="test_on_combined_data")
+#still to run:
 
 train_data = 'data'
 test_data = 'switchboard_data'
@@ -20,10 +15,25 @@ trial_path = './no_subnets/2_Acous_10ms'
 test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data,
                         report_dict_name="test_on_switchboard_data")
 
-trial_path = './no_subnets/3_Ling_50ms'
-test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data,
-                        report_dict_name="test_on_switchboard_data")
+# train_data = 'data'
+# test_data = 'both_data'
+# trial_path = './no_subnets/3_Ling_50ms'
+# test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data,
+#                         report_dict_name="test_on_combined_data")
+# train_data = 'data'
+# test_data = 'both_data'
+# trial_path = './two_subnets/2_Acous_10ms_Ling_50ms'
+# test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data,
+#                         report_dict_name="test_on_combined_data")
 
-trial_path = './two_subnets/2_Acous_10ms_Ling_50ms'
-test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data,
-                        report_dict_name="test_on_switchboard_data")
+
+train_data = 'data'
+test_data = 'switchboard_data'
+# trial_path = './no_subnets/3_Ling_50ms'
+# test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data,
+#                         report_dict_name="test_on_switchboard_data")
+train_data = 'data'
+test_data = 'switchboard_data'
+# trial_path = './two_subnets/2_Acous_10ms_Ling_50ms'
+# test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data,
+#                         report_dict_name="test_on_switchboard_data")

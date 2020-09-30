4729874f65b74920e91e163dff3c16458bcd6255
sarahburnejames <sarahmbj@gmail.com>
2020-09-30 12:37:27 +0100

    comment out unneeded tests

--- maptask_unk_tests.py
+++ maptask_unk_tests.py
@@ -3,13 +3,14 @@ from test_on_existing_model import test_on_existing_models
 # cross corpus tests - acoustic only models
 trial_path = './no_subnets/2_Acous_10ms'
 
-train_data = 'data'
-test_data = 'switchboard_data'
-test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data)
-
+# train_data = 'data'
+# test_data = 'switchboard_data'
+# test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data)
+#
 train_data = 'data'
 test_data = 'both_data'
-test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data)
+test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data,
+                        report_dict_name="test_on_combined_data")
 
 
 # trial_path = './f_and_g_no_subnets/4_Acous_10ms_gtrain'
@@ -48,14 +49,14 @@ test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_di
 #                         test_on_f=True, test_on_g=True, report_dict_name=f'test_on_{test_data}')
 
 
-train_data = 'data'
-test_data = 'switchboard_data'
-trial_path = './no_subnets/3_Ling_50ms'
-test_on_existing_models(trial_path, test_data_dir=test_data, train_data_dir=train_data,
-                        test_on_f=True, test_on_g=True, report_dict_name=f'test_on_{test_data}')
-trial_path = './two_subnets/2_Acous_10ms_Ling_50ms'
-test_on_existing_models(trial_path, test_data_dir=test_data, train_data_dir=train_data,
-                        test_on_f=True, test_on_g=True, report_dict_name=f'test_on_{test_data}')
+# train_data = 'data'
+# test_data = 'switchboard_data'
+# trial_path = './no_subnets/3_Ling_50ms'
+# test_on_existing_models(trial_path, test_data_dir=test_data, train_data_dir=train_data,
+#                         test_on_f=True, test_on_g=True, report_dict_name=f'test_on_{test_data}')
+# trial_path = './two_subnets/2_Acous_10ms_Ling_50ms'
+# test_on_existing_models(trial_path, test_data_dir=test_data, train_data_dir=train_data,
+#                         test_on_f=True, test_on_g=True, report_dict_name=f'test_on_{test_data}')
 
 train_data = 'data'
 test_data = 'both_data'

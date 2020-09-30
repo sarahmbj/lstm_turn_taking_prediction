e5a996cbedd113a7d662755a9d2c5b21bde594e6
sarahburnejames <sarahmbj@gmail.com>
2020-09-30 11:44:39 +0100

    comment out unneeded tests

--- maptask_unk_tests.py
+++ maptask_unk_tests.py
@@ -12,29 +12,29 @@ test_data = 'both_data'
 test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data)
 
 
-trial_path = './f_and_g_no_subnets/4_Acous_10ms_gtrain'
-
-train_data = 'data'
-test_data = 'switchboard_data'
-test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, trained_on_f=False, trained_on_g=True,
-                        test_data_dir=test_data, train_data_dir=train_data)
-
-train_data = 'data'
-test_data = 'both_data'
-test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, trained_on_f=False, trained_on_g=True,
-                        test_data_dir=test_data, train_data_dir=train_data)
-
-trial_path = './f_and_g_no_subnets/3_Acous_10ms_ftrain'
-
-train_data = 'data'
-test_data = 'switchboard_data'
-test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, trained_on_f=True, trained_on_g=False,
-                        test_data_dir=test_data, train_data_dir=train_data)
-
-train_data = 'data'
-test_data = 'both_data'
-test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, trained_on_f=True, trained_on_g=False,
-                        test_data_dir=test_data, train_data_dir=train_data)
+# trial_path = './f_and_g_no_subnets/4_Acous_10ms_gtrain'
+#
+# train_data = 'data'
+# test_data = 'switchboard_data'
+# test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, trained_on_f=False, trained_on_g=True,
+#                         test_data_dir=test_data, train_data_dir=train_data)
+#
+# train_data = 'data'
+# test_data = 'both_data'
+# test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, trained_on_f=False, trained_on_g=True,
+#                         test_data_dir=test_data, train_data_dir=train_data)
+#
+# trial_path = './f_and_g_no_subnets/3_Acous_10ms_ftrain'
+#
+# train_data = 'data'
+# test_data = 'switchboard_data'
+# test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, trained_on_f=True, trained_on_g=False,
+#                         test_data_dir=test_data, train_data_dir=train_data)
+#
+# train_data = 'data'
+# test_data = 'both_data'
+# test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, trained_on_f=True, trained_on_g=False,
+#                         test_data_dir=test_data, train_data_dir=train_data)
 
 
 # models trained on both roles

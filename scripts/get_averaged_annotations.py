d914395d9665d70ececcebca79208c1e0700e2a6
sarahburnejames <sarahmbj@gmail.com>
2020-10-01 14:41:17 +0100

    change frozenset to set

--- scripts/get_averaged_annotations.py
+++ scripts/get_averaged_annotations.py
@@ -26,12 +26,14 @@ if speed_setting == 0:
     path_to_orig_embeds = f'./{dataset}/extracted_annotations/words_advanced_50ms_raw/'
     path_to_extracted_annotations = f'./{dataset}/extracted_annotations/words_advanced_50ms_averaged/'
     output_set_dict = f'./{dataset}/extracted_annotations/set_dict_50ms.p'
+    speed_string = "50ms"
 
 elif speed_setting == 1:
     path_to_features = f'./{dataset}/signals/gemaps_features_processed_10ms/znormalized/'
     path_to_orig_embeds = f'./{dataset}/extracted_annotations/words_advanced_10ms_raw/'
     path_to_extracted_annotations = f'./{dataset}/extracted_annotations/words_advanced_10ms_averaged/'
     output_set_dict = f'./{dataset}/extracted_annotations/set_dict_10ms.p'
+    speed_string = "10ms"
 
 
 def find_nearest(array, value):
@@ -52,7 +54,6 @@ for file in files_feature_list:
     files_output_list.append(os.path.splitext(base_name)[0]+'.csv')
 
 word_to_ix = pickle.load(open('./data/extracted_annotations/word_to_ix.p', 'rb'))
-# glove_embed_table = pickle.load(open('./extracted_annotations/glove_embed_table.p','rb'))
 
 # Create delayed frame annotations
 max_len = 0
@@ -63,7 +64,7 @@ for i in range(0, len(files_feature_list)):
     orig_file = pd.read_csv(path_to_orig_embeds+files_feature_list[i], delimiter=',')
     combins = np.array(orig_file[orig_file.columns[1:]])[list(set(np.nonzero(np.array
                                                                              (orig_file[orig_file.columns[1:]]))[0]))]
-    local_set = [frozenset(i) for i in combins]
+    local_set = [set(i) for i in combins]
     total_list.extend(local_set)
 
 total_set = set(total_list)
@@ -73,25 +74,26 @@ set_dict, glove_embed_dict_50ms = {}, {}
 for indx, glove_combination in enumerate(total_set):
     set_dict[glove_combination] = indx+1
 
-set_dict[frozenset([0])] = 0
+set_dict[set([0])] = 0
 
 # get new word_reg annotations for new embedding dict
 for i in range(0, len(files_feature_list)):
 
     print('percent done files create:' + str(i/len(files_feature_list))[0:4])
-    orig_file = pd.read_csv(path_to_orig_embeds+files_feature_list[i], delimiter=',')
+    orig_file = pd.read_csv(path_to_orig_embeds + files_feature_list[i], delimiter=',')
     frame_times = orig_file['frameTimes']
     word_annotations = np.zeros(frame_times.shape)
     indices = list(set(np.nonzero(np.array(orig_file[orig_file.columns[1:]]))[0]))
     for indx in indices:
-        word_annotations[indx] = set_dict[frozenset(np.array(orig_file[orig_file.columns[1:]])[indx])]
+        word_annotations[indx] = set_dict[set(np.array(orig_file[orig_file.columns[1:]])[indx])]
     output = pd.DataFrame(np.vstack([frame_times, word_annotations]).transpose())
     output.columns = ['frameTimes', 'word']
     output.to_csv(path_to_extracted_annotations + files_output_list[i], float_format='%.6f', sep=',', index=False,
                   header=True)
 
 pickle.dump(set_dict, open(output_set_dict, 'wb'))
-json.dump(set_dict, open(f'./{dataset}/extracted_annotations/output_dict_averaged_annotations.json', 'w'), indent=4)
+json.dump(set_dict, open(f'./{dataset}/extracted_annotations/output_dict_averaged_annotations_{speed_string}.json',
+                         'w'), indent=4)
 
 print('total_time: ' + str(t.time()-t_1))
 

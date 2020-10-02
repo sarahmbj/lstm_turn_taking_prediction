d825bcfe6bdbb41148357902afd41da0afaaa304
sarahburnejames <sarahmbj@gmail.com>
2020-10-02 12:10:00 +0100

    correct report dict names

--- switchboard_unk_cross_corp_tests.py
+++ switchboard_unk_cross_corp_tests.py
@@ -8,7 +8,7 @@ train_data = 'data' #
 test_data = 'maptask_data'
 trial_path = './no_subnets/2_Acous_10ms'
 test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data,
-                        report_dict_name="test_on_combined_data")
+                        report_dict_name="test_on_maptask_data")
 
 train_data = 'data'
 test_data = 'both_data'
@@ -34,10 +34,10 @@ train_data = 'data'
 test_data = 'maptask_data'
 trial_path = './two_subnets/2_Acous_10ms_Ling_50ms'
 test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data,
-                        report_dict_name="test_on_combined_data")
+                        report_dict_name="test_on_maptask_data")
 
 train_data = 'data'
 test_data = 'maptask_data'
 trial_path = './no_subnets/3_Ling_50ms'
 test_on_existing_models(trial_path, test_on_f=True, test_on_g=True, test_data_dir=test_data, train_data_dir=train_data,
-                        report_dict_name="test_on_combined_data")
+                        report_dict_name="test_on_maptask_data")
